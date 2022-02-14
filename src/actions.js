6732a6166410b91e652a7092e1b6680c3b978883
Zmushko Yulia <lobanyulia1991@gmail.com>
2022-02-15 01:18:11 +0300

    first commit

---
+++ src/actions.js
@@ -0,0 +1,11 @@
+export const inc = () => ({ type: 'INC' });
+
+export const dec = () => ({ type: 'DEC' });
+
+export const rnd = (payload) => {
+
+  return {
+     type: 'RND',
+     payload: Math.floor(Math.random()*10)
+  };
+};
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import App from './components/app';
+
+import reducer from './reducer';
+
+
+const store = createStore(reducer);
+
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        document.getElementById('root'));
+
